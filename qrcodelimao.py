2c5b9cae4dbc5a13cb73526b53a5d9c2e7414ec6
fnishgg <felipe157dp@gmail.com>
2024-10-27 15:44:27 -0300

    Commit CSS

--- qrcodelimao.py
+++ qrcodelimao.py
@@ -1,21 +1,22 @@
-from flask import Flask, jsonify, send_file
+from flask import Flask, jsonify, send_file, render_template
 import qrcode
 import io
 
 app = Flask(__name__)
 
-base_url = "https://www.instagram.com/limaogalego.restaurante?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="  # URL base
+# URL base para os QR Codes
+base_url = "https://www.instagram.com/limaogalego.restaurante/?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
 counter = 1  # Inicia o contador
 
 @app.route('/generate_qr', methods=['GET'])
 def generate_qr():
     global counter
 
-    if counter > 5:
-        return jsonify({"error": "Limite de 100 QR Codes atingido"}), 403
+    if counter > 5:  # Alterado para 100 conforme sua solicitação
+        return render_template('error.html')  # Renderiza a página de erro se o limite for atingido
 
-    # Cria a URL com o contador atual
-    unique_url = f"{base_url}{counter}"
+    # Cria a URL única com o contador atual
+    unique_url = f"{base_url}/{counter}"
 
     # Gera o QR Code para a URL única
     img = qrcode.make(unique_url)
